adcfded866edc7cd3eca75b16d12288b0d1acd0f
ottdog <55850761+ottdog@users.noreply.github.com>
2022-03-14 22:55:24 -0400

    Add files via upload

---
+++ labs/lab6/letterAnywhereWordLabrynth.py
@@ -0,0 +1,109 @@
+
+# Modification of the words/latter graph to that creates an 
+# edge between words that share a letter regardless of positiion
+# By Otto Daniels for Open Source Software at RPI spring 2022.
+
+"""
+=====
+Words
+=====
+Words/Ladder Graph
+------------------
+Generate  an undirected graph over the 5757 5-letter words in the
+datafile `words_dat.txt.gz`.  Two words are connected by an edge
+if they differ in one letter, resulting in 14,135 edges. This example
+is described in Section 1.1 in Knuth's book (see [1]_ and [2]_).
+References
+----------
+.. [1] Donald E. Knuth,
+   "The Stanford GraphBase: A Platform for Combinatorial Computing",
+   ACM Press, New York, 1993.
+.. [2] http://www-cs-faculty.stanford.edu/~knuth/sgb.html
+"""
+# Authors: Aric Hagberg (hagberg@lanl.gov),
+#          Brendt Wohlberg,
+#          hughdbrown@yahoo.com
+
+#    Copyright (C) 2004-2019 by
+#    Aric Hagberg <hagberg@lanl.gov>
+#    Dan Schult <dschult@colgate.edu>
+#    Pieter Swart <swart@lanl.gov>
+#    All rights reserved.
+#    BSD license.
+
+import gzip
+from string import ascii_lowercase as lowercase
+
+import networkx as nx
+
+#-------------------------------------------------------------------
+#   The Words/Ladder graph of Section 1.1
+#-------------------------------------------------------------------
+
+
+def generate_graph(words):
+    G = nx.Graph(name="words")
+    lookup = dict((c, lowercase.index(c)) for c in lowercase)
+         
+    def share_all_but_one_letter(word1, word2):
+        letterList1 = []
+        letterList2 = []
+        
+        for i in range(len(word1)):
+            letterList1.append(word1[i])
+            letterList2.append(word2[i])
+            
+        sortedLL1 = sorted(letterList1)
+        sortedLL2 = sorted(letterList2)
+    
+        x = 0
+        for i in range(len(word1)):
+            if sortedLL1[i] == sortedLL2[i]:
+                x = x + 1
+        return x == 4
+            
+    G.add_nodes_from(words)
+        
+    for i in sorted(words):
+        #print (i)
+        for j in sorted(words):
+            
+            if share_all_but_one_letter(i, j):
+                G.add_edge(i, j)
+    return G
+
+
+def words_graph():
+    """Return the words example graph from the Stanford GraphBase"""
+    fh = gzip.open('words_dat.txt.gz', 'r')
+    words = set()
+    for line in fh.readlines():
+        line = line.decode()
+        if line.startswith('*'):
+            continue
+        w = str(line[0:5])
+        words.add(w)
+    return generate_graph(words)
+
+
+if __name__ == '__main__':
+    G = words_graph()
+    print("Loaded words_dat.txt containing a bunch of five-letter English words.")
+    print("Two words are connected if they differ in one letter.")
+    print("Graph has %d nodes with %d edges"
+          % (nx.number_of_nodes(G), nx.number_of_edges(G)))
+    print("%d connected components" % nx.number_connected_components(G))
+
+    for (source, target) in [('chaos', 'order'),
+                             ('plots', 'graph'),
+                             ('moron', 'smart'),
+                             ('flies', 'swims'),
+                             ('mango', 'peach'),
+                             ('pound', 'marks')]:
+        print("Shortest path between %s and %s is" % (source, target))
+        try:
+            sp = nx.shortest_path(G, source, target)
+            for n in sp:
+                print(n)
+        except nx.NetworkXNoPath:
+            print("None")
